74e118ff57be4b62e6ddad397e4b3091d9ce9344
pablorcruh <pablorcruh@gmail.com>
2021-01-25 17:31:14 -0500

    update and delete product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -12,6 +12,7 @@ export const createProduct = async (req, res) => {
         const productSaved = await newProduct.save()
         res.status(200).json(productSaved)
     } catch (error) {
+        console.error.error(error.message)
         res.status(500).json({message: error.message})
     }
 }
@@ -28,6 +29,7 @@ export const uploadImage = async (req, res) => {
         await Products.findByIdAndUpdate(productId, product)
         res.status(200).send()
     }catch(error){
+        console.error.error(error.message)
         res.status(500).json({message: 'Oops something went wrong'})
     }
 }
@@ -39,26 +41,38 @@ export const getProducts = async (req, res) => {
 export const getProductById = async (req, res) => {
     try{
         const productId = req.params.productId
-        const product = await Products.findById(productId)
+        const product =  Products.findById(productId)
         if(!product){
             res.status(400).json({message: 'product not found'})
             return
         }
         res.status(200).json(product) 
     }catch(error){
-        console.error(error.message)
+        console.error.error(error.message)
         res.status(500).json({message:'Oops something went wrong'})
     }
 }
 
 export const updateProductById = async (req, res) => {
-    
+    try{
+        const productId = req.params.productId
+        const updatedProduct = await Products.findByIdAndUpdate(productId, req.body, {
+            new: true
+        })
+        res.status(200).json(updatedProduct)
+    }catch(error){
+        console.error(error.message)
+        res.status(500).json({message: 'Oops something went wrong'})
+    }
 }
 
 export const deleteProductById = async (req, res) => {
-
-}
-
-export const uploadProductImage = async (req, res) => {
-
+    try {
+        const productId = req.params.productId
+        await Products.findByIdAndDelete(productId)
+        res.status(201).send()       
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Oops something went wrong'})
+    }
 }
