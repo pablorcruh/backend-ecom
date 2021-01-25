276c732ec7dcf1dcd4717712c7ec144cc2050665
pablorcruh <pablorcruh@gmail.com>
2021-01-25 18:01:49 -0500

    add Users model

---
+++ src/models/Users.js
@@ -0,0 +1,34 @@
+import {Schema, model} from 'mongoose'
+import validator from 'validator'
+const usersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true        
+    },
+    username: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate(value){
+            if(validator.isLength(value, {min: 5})){
+                throw new Error('Password must be greated than 5')
+            }
+        }
+    }
+})
+
+export default model('user', usersSchema)
